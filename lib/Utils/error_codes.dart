072f412d143489fe2e5a684a7a597981fb1f2f20
Ho Yi Liang <yiliangmc@gmail.com>
2023-06-11 00:05:35 +0800

    + Reimplement Encryptor + New error codes for Registration + Refine API calls + Remove Address from User entity

--- lib/Utils/error_codes.dart
+++ lib/Utils/error_codes.dart
@@ -2,4 +2,6 @@ class ErrorCodes {
   static final String OPERATION_OK = 'E-000';
   static final String LOGIN_FAIL_NO_USER = 'E-001';
   static final String LOGIN_FAIL_API_CONNECTION = 'E-002';
+  static final String REGISTER_FAIL_USER_EXISTS = 'E-003';
+  static final String REGISTER_FAIL_API_CONNECTION = 'E-004';
 }
