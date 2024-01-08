6477fb5a36c9a92d16773a61c55fbe45587cc1a7
Goh Chee Lam <clgoh0726@gmail.com>
2024-01-08 21:28:03 +0800

    Done: Add the ip address env

--- lib/Utils/ip_address.dart
+++ lib/Utils/ip_address.dart
@@ -1,3 +1,4 @@
 class IpAddress {
-  static const String ip_addr = 'http://keninacafe.tplinkdns.com:8000';
+  // static const String ip_addr = 'http://keninacafe.tplinkdns.com:8000';
+  static const String ip_addr = 'https://keninacafe-api-99052c13b7b0.herokuapp.com';
 }
